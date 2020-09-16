912cd1414af86e3f07d40a9ca5ca4047e373a969
Jacky Ye <yeqw321@163.com>
2020-09-16 14:22:17 +0800

    提交

---
+++ WebApplication2/App_Start/WebApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebApplication2
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API 配置和服务
+            var globalCors = new EnableCorsAttribute("*", "*", "*")
+            {
+                SupportsCredentials = true
+            };
+            config.EnableCors(globalCors);
+            // Web API 路由
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+        }
+    }
+}
---
+++ WebApplication2/Controllers/ValuesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Speech.AudioFormat;
+using System.Speech.Synthesis;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Http;
+
+namespace WebApplication2.Controllers
+{
+    [RoutePrefix("api/TTS")]
+    public class TTSController : ApiController
+    {
+
+        [HttpGet, Route("GetPath")]
+        public HttpResponseMessage GetPath(string Text, int rate = -2)
+        {
+            try
+            {
+                string phrase = Text;
+                var name = Guid.NewGuid().ToString("N");
+                var path = HostingEnvironment.MapPath(@"~\mp3\");
+                var truepath = path + name + ".wav";
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                var t = new System.Threading.Thread(() =>
+                {
+                    using (var synth = new SpeechSynthesizer())
+                    {
+                        synth.Rate = rate;
+                        synth.Volume = 100;
+                        synth.SetOutputToWaveFile(truepath, new SpeechAudioFormatInfo(8000, AudioBitsPerSample.Eight, AudioChannel.Mono));
+                        synth.Speak(phrase);
+                        synth.SetOutputToNull();
+                    }
+                });
+
+                t.Start();
+                t.Join();
+
+                var dataBytes = File.ReadAllBytes(truepath);
+                var dataStream = new MemoryStream(dataBytes);
+                var resp = new HttpResponseMessage(HttpStatusCode.OK);
+                resp.Content = new StreamContent(dataStream);
+
+                resp.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("audio/wav");
+                resp.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment");
+                resp.Content.Headers.ContentDisposition.FileName = name + ".wav";
+                File.Delete(truepath);
+                return resp;
+            }
+            catch
+            {
+                return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            }
+        }
+
+    }
+}
